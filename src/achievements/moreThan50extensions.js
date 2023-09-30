e877b742f9c988948c0d42b18b50d7463bfe0e8a
0adiy <adityaruva@gmail.com>
2023-09-30 12:58:49 +0530

    added 2 more ach

---
+++ src/achievements/moreThan50extensions.js
@@ -0,0 +1,21 @@
+const vscode = require("vscode");
+
+const name = "moreThan50extensions";
+
+module.exports = async (achList, updateAchList) => {
+  if (achList.includes(name)) return;
+
+  const dispoable = vscode.workspace.onDidChangeWorkspaceFolders(event => {
+    // Get the number of currently loaded extensions
+    const numExtensions = vscode.extensions.all.length;
+
+    // Check if the number of extensions is above 50
+    if (numExtensions > 50) {
+      vscode.window.showInformationMessage(
+        "🏆Achievement Unlocked🔓: Hoarder: You have more than 50 extensions loaded!📝"
+      );
+      updateAchList(name);
+      dispoable.dispose();
+    }
+  });
+};
---
+++ src/achievements/usingGitFirstTime.js
@@ -0,0 +1,27 @@
+const vscode = require("vscode");
+const gitExtension = vscode.extensions.getExtension("vscode.git").exports;
+const git = gitExtension.getAPI(1);
+
+const name = "usingGitFirstTime";
+
+module.exports = async (achList, updateAchList) => {
+  if (achList.includes(name)) return;
+
+  const dispoable = vscode.workspace.onDidChangeWorkspaceFolders(event => {
+    // Get the current workspace
+    const workspace = vscode.workspace.workspaceFolders[0].uri.fsPath;
+
+    // Find the repository corresponding to the workspace
+    const repo = git.repositories.find(
+      repository => repository.rootUri.fsPath === workspace
+    );
+
+    if (repo) {
+      vscode.window.showInformationMessage(
+        "🏆Achievement Unlocked🔓: Version Control📝"
+      );
+      updateAchList(name);
+      dispoable.dispose();
+    }
+  });
+};
