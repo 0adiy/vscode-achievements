e6bb4d0e28822872ed0eeccc77d00af0dc40ff32
Harsh-bhai <107921110+Harsh-bhai@users.noreply.github.com>
2023-09-30 13:48:04 +0530

    multiple lanuguage coder achievement created

---
+++ src/achievements/firstFormatDocument.js
@@ -0,0 +1,23 @@
+const vscode = require("vscode");
+
+const name = "firstFormatDocument";
+
+module.exports = (achList, updateAchList) => {
+  if (achList.includes(name)) return;
+
+  let formatDocumentUsed = false;
+
+  const disposable = vscode.workspace.onDidChangeTextDocument((event) => {
+    //@ts-ignore
+    if (event.reason === vscode.TextDocumentSaveReason.AfterDelay) {
+      // This event is triggered when the document is saved.
+      if (!formatDocumentUsed) {
+        // "Format Document" has not been used before this save.
+        vscode.window.showInformationMessage("You formatted the document for the first time!");
+        updateAchList(name);
+        formatDocumentUsed = true;
+        disposable.dispose();
+      }
+    }
+  });
+};
--- src/achievements/multiLingual.js
+++ src/achievements/multiLingual.js
@@ -16,16 +16,30 @@ function updateUniqueLanguages() {
 }
 
 const name = "languageMaster";
-
+/**
+ * 
+ * @param {vscode.ExtensionContext} context 
+ * @param {*} achList 
+ * @param {*} updateAchList 
+ * @returns 
+ */
 module.exports = (context, achList, updateAchList) => {
+  const languageSet = context.globalState.get("languageSet", new Set());
   if (achList.includes(name)) return;
 
-  updateUniqueLanguages();
+  const disposable = vscode.workspace.onDidOpenTextDocument((document) => {
+    // Get the language ID of the opened file
+    const languageId = document.languageId;
+    languageSet.add(languageId);
+    if(languageSet.size>2){
+      vscode.window.showInformationMessage(
+        "🏆Achievement Unlocked🔓: Top 1% Coder📈"
+      );
+      updateAchList(name);
+      disposable.dispose();
+    }
 
-  // Listen for document open events to update the unique languages
-  const disposable = vscode.workspace.onDidOpenTextDocument(() => {
-    updateUniqueLanguages();
+    // Save the updated set back to globalState
+    context.globalState.update("languageSet", languageSet);
   });
-
-  disposable.dispose();
 };
