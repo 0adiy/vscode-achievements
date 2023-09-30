19d16ccfb5b3a7cb1f7c2d2a65f1fc0b35154c00
Harsh-bhai <107921110+Harsh-bhai@users.noreply.github.com>
2023-09-30 19:07:01 +0530

    adha kam hogya

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 const vscode = require("vscode");
 const loadAchievements = require("./src/functions/loadAchievements");
-const getWebView = require("./src/functions/getWebView")
+const getWebView = require("./src/functions/getWebView");
 
 let isActivated = false;
 
@@ -14,6 +14,36 @@ async function activate(context) {
     context.globalState.update("achivements", []);
     let achList = context.globalState.get("achivements") || [];
 
+    // Create a custom icon for your button (you can use SVG or PNG)
+    const customIcon = vscode.Uri.file(
+      context.asAbsolutePath("./src/assets/medal.png")
+    );
+    
+
+    // Create a custom title for your button
+    const customTitle = "Achievements";
+
+    // Create a command ID for your button
+    const commandId = "extension.achivements";
+
+    // Register a command for your button
+    vscode.commands.registerCommand(commandId, () => {
+      vscode.window.showInformationMessage(
+        "🏆command activated"
+      );
+      createWebView();
+      // Create and add the button to the Activity Bar
+      const customButton = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left
+      );
+      customButton.text = customTitle;
+      customButton.tooltip = customTitle;
+      customButton.command = commandId;
+      // customButton.icon = customIcon;
+      // Show the button in the Activity Bar
+      customButton.show();
+    });
+
     function updateAchList(input) {
       achList.push(input);
       context.globalState.update("achivements", achList);
@@ -25,7 +55,6 @@ async function activate(context) {
     for (const ach of achivements) ach(context, achList, updateAchList);
 
     // Create a webview panel for the custom HTML
-    createWebView();
 
     isActivated = true;
   }
@@ -45,7 +74,6 @@ function createWebView() {
       enableScripts: true, // Enable JavaScript in the webview
     }
   );
-
 
   // Set the HTML content in the webview
   panel.webview.html = getWebView();
